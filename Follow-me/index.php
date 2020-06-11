6e1bb0cc54b8ed3fbed2150a11ebf25fa07280c8
Anushka Virgaonkar <anushka18599@gmail.com>
2020-06-11 11:49:06 +0530

    Update index.php

--- Follow-me/index.php
+++ Follow-me/index.php
@@ -43,7 +43,7 @@ ob_end_flush();
   <button type="submit" class="btn btn-primary">Submit</button>
 </form>
 	    
-<!-- It is advised that no file of file type other than .php must reside on the webserver -->
+
 	    
 </div>
     <div class="col" style="margin-left: 180px;">
