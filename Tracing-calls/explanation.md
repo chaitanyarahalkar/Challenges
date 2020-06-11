cc735ea46dfff812e49e63af4e5af3608a3b7c95
Anushka Virgaonkar <anushka18599@gmail.com>
2020-06-11 23:13:27 +0530

    Add challenge

---
+++ Tracing-calls/explanation.md
@@ -0,0 +1,14 @@
+## Problem: Tracing-calls
+
+To know the password, try checking the calls that this executable makes.
+
+## Hint: 
+ltrace
+
+## Explanation:
+
+Running:
+`ltrace ./script`
+Shows that the password is being compared with s63bdnd9nbd83rg89cb939f3. Entering that as our password, we get the flag.
+
+Flag: `lakshya_CTF{ac3d_lr4c3}`
---
+++ Tracing-calls/script.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+
+#define BUFSIZE 148
+#define FLAGSIZE 128
+
+int flag(char *buf){
+  char flag[48];
+  FILE *file;
+  file = fopen("flag.txt", "r");
+  if (file == NULL) {
+    printf("Flag File is Missing. Problem is Misconfigured, please contact an Admin if you are running this on the shell server.\n");
+    exit(0);
+  }
+
+  fgets(flag, sizeof(flag), file);
+  printf("%s", flag);
+  return 0;
+}
+
+int main(int argc, char **argv){
+
+  setvbuf(stdout, NULL, _IONBF, 0);
+  
+  // Set the gid to the effective gid
+  // this prevents /bin/sh from dropping the privileges
+  gid_t gid = getegid();
+  setresgid(gid, gid, gid);
+
+  char password[64];
+
+  puts("Enter the password!");
+  if (!strcmp("s63bdnd9nbd83rg89cb939f3", password)) {
+    flag();
+  }
+     
+  return 0;
+}
